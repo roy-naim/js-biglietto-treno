f5bc1e23e3bfd63d4922c22a070a3f322ca6613e
Roy <b.roynaim@gmail.com>
2021-03-01 17:02:38 +0100

    final ticket price completed, missing conditions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 alert("Welcome to InterTrain. Please enter your detalils to check your ticket final price. Thank you.");
 
-var kilometers = prompt("How many km you need to travel?")
+var kilometers = prompt("How many km you need to travel?");
 
-var age = prompt("How old are you?")
+var age = prompt("How old are you?");
 
 var price = 0.21;
 
 var final = kilometers * price;
 
 console.log(final);
+
+alert("Your Final Ticket price is " + final + "€");
