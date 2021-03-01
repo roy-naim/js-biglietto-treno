71e5a78959ed79e22bbdd0306c9480a4f62477a0
Roy <b.roynaim@gmail.com>
2021-03-01 16:54:24 +0100

    js created

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+alert("Welcome to InterTrain. Please enter your detalils to check your ticket final price. Thank you.");
+
+var kilometers = prompt("How many km you need to travel?")
+
+var age = prompt("How old are you?")
+
+var price = 0.21;
+
+var final = kilometers * age;
+
+console.log(final);
