d32a2e927b1ce623512cb0ae93b0acd6501226b9
Roy <b.roynaim@gmail.com>
2021-03-01 17:38:28 +0100

    final price check with if

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,20 @@ alert("Welcome to InterTrain. Please enter your detalils to check your ticket fi
 
 var kilometers = prompt("How many km you need to travel?");
 
-var age = prompt("How old are you?");
+var age = parseInt(prompt("How old are you?"));
 
 var price = 0.21;
 
 var final = kilometers * price;
 
-console.log(final);
+// console.log(final);
+//
+// alert("Your Final Ticket price is " + final + "€");
 
-alert("Your Final Ticket price is " + final + "€");
+if (age < 18) {
+  alert("Your Final Ticket price is " + (final - ((final * 20) /100) + "€"));
+} else if (age > 65) {
+  alert("Your Final Ticket price is " + (final - (final * 40) /100 + "€"))
+} else {
+  alert("Your Final Ticket price is " + final + "€")
+}
